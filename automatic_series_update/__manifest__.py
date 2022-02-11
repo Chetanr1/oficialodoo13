6b6c01419bfe167b267aade8407412dd75ac7f4d
Jesus <jesus_sanchezjibaja@hotmail.com>
2022-02-11 10:58:48 -0500

    url automatic y correcion series

--- automatic_series_update/__manifest__.py
+++ automatic_series_update/__manifest__.py
@@ -3,13 +3,13 @@
 	'name': 'Automatic Series Update',
 	'category': 'account',
 	'author': 'ITGRUPO',
-	'depends': ['account_fields_it'],
+	'depends': ['account_fields_it','base'],
 	'version': '1.0',
 	'description':"""
 	Automatizacion de Referencias en Base a la Serie
 	""",
 	'auto_install': False,
 	'demo': [],
-	'data':	[],
+	'data':	['ir.model.access.csv'],
 	'installable': True
 }
--- automatic_series_update/models/account_move.py
+++ automatic_series_update/models/account_move.py
@@ -26,14 +26,18 @@ class AccountMove(models.Model):
 		#for self in selfs:
 		if name == '/':
 			if self.serie_id.sequence_id:
-				next_number = self.serie_id.sequence_id.next_by_id()
+				#funcion maldita
+				#next_number = self.serie_id.sequence_id.next_by_id()
 				if not self.serie_id.sequence_id.prefix:
 					raise UserError("No existe un prefijo configurado en la secuencia de la serie.")
-				# prefix = self.serie_id.sequence_id.prefix
-				# padding = self.serie_id.sequence_id.padding
-				# self.ref = prefix + "0"*(padding - len(str(next_number))) + str(next_number)
-				# self.serie_id.sequence_id.next_by_id()
-				self.ref = next_number
+				sequence = self.serie_id.sequence_id
+				next_number =sequence.number_next_actual
+				serie = str(next_number).rjust(sequence.padding, '0')
+				serie = (sequence.prefix or '') + serie + (sequence.suffix or '')
+				name = serie
+
+				sequence.number_next_actual = next_number + self.serie_id.sequence_id.number_increment
+				self.ref = name
 
 		return res
 
